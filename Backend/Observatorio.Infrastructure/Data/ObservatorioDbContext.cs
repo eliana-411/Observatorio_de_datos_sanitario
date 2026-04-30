8cc3cb5fd9f8555989b43bdcc252526af38b4b57
eliana-411 <eliana04112001@gmail.com>
2026-04-29 21:18:47 -0500

    2FA

--- Backend/Observatorio.Infrastructure/Data/ObservatorioDbContext.cs
+++ Backend/Observatorio.Infrastructure/Data/ObservatorioDbContext.cs
@@ -37,7 +37,23 @@ public class ObservatorioDbContext : DbContext
         
         modelBuilder.Entity<User>().Property(u => u.Role)
             .HasColumnName("rol");
-        
+
+        modelBuilder.Entity<User>().Property(u => u.RefreshToken)
+            .HasColumnName("refresh_token");
+
+        modelBuilder.Entity<User>().Property(u => u.RefreshTokenExpiryDate)
+            .HasColumnName("refresh_token_expiry_date");
+
+        modelBuilder.Entity<User>().Property(u => u.IsTwoFactorEnabled)
+            .HasColumnName("is_two_factor_enabled")
+            .HasDefaultValue(true);
+
+        modelBuilder.Entity<User>().Property(u => u.TwoFactorCode)
+            .HasColumnName("two_factor_code");
+
+        modelBuilder.Entity<User>().Property(u => u.TwoFactorCodeExpiry)
+            .HasColumnName("two_factor_code_expiry");
+
         modelBuilder.Entity<User>().Ignore(u => u.Provider);
         modelBuilder.Entity<User>().Ignore(u => u.ProviderId);
         
